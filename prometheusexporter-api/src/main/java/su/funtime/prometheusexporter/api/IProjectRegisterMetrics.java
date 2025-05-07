24a55bd54a125248e834cd7fbbd6d7372794bf0f
Nikita Huliuk <nikaron4ik@gmail.com>
2025-05-07 19:11:01 +0300

    Edited metric interface. Added registerMetric() method, edited JavaDoc

--- prometheusexporter-api/src/main/java/su/funtime/prometheusexporter/api/IProjectRegisterMetrics.java
+++ prometheusexporter-api/src/main/java/su/funtime/prometheusexporter/api/IProjectRegisterMetrics.java
@@ -4,23 +4,44 @@ import io.prometheus.client.Gauge;
 import java.util.function.Supplier;
 import org.bukkit.plugin.Plugin;
 
+/**
+ * Интерфейс для регистрации и сбора метрик через Prometheus
+ */
 public interface IProjectRegisterMetrics {
+
     /**
+     * Создаёт Gauge-метрику без регистрации её значений.
+     * @deprecated Рекомендуется использовать {@link #registerMetric}
      *
-     * @param name - Название метрики (автоматически создаётся префиксом mc_)
-     * @param help - Строка справки для метрики
-     * @return - Gauge значение, которое динамически обновляется при каждом сборе метрики
+     * @param name Уникальное название метрики
+     * @param help Описание назначения метрики
+     * @return {@link Gauge} (для сбора метрики должен быть зарегистрирован через {@link #collectMetric})
      */
     Gauge gaugeBuilder(String name, String help);
 
 
     /**
+     * Регистрирует сбор Gauge-метрики
+     * @deprecated Рекомендуется использовать {@link #registerMetric}
      *
-     * @param plugin - плагин, в котором собирается метрика
-     * @param gauge - Gauge значение (создаётся методом gaugeBuilder)
-     * @param supplier - Поставщик значения (метод), возвращающий актуальное значение метрики при каждом сборе (только Double)
-     * @param isAsync - определяет, асинхронно (true) или синхронно (false) будет собираться метрика
+     * @param plugin Плагин, в котором собирается метрика
+     * @param gauge Gauge значение, создаётся при помощи gaugeBuilder
+     * @param supplier Поставщик значения (метод), возвращающий актуальное значение метрики при каждом сборе
+     * @param isAsync true для асинхронного сбора метрики, false для синхронного
      */
     void collectMetric(Plugin plugin, Gauge gauge, Supplier<Double> supplier, boolean isAsync);
 
+
+    /**
+     * Создаёт и сразу регистрирует сбор Gauge-метрики <b>(рекомендуемый метод) </b>
+     *
+     * @param plugin Плагин, в котором собирается метрика
+     * @param name Название метрики (уникальное)
+     * @param help Описание метрики
+     * @param supplier Поставщик значения (метод), возвращающий актуальное значение метрики при каждом сборе
+     * @param isAsync true для асинхронного сбора метрики, false для синхронного
+     */
+    default void registerMetric(Plugin plugin, String name, String help, Supplier<Double> supplier, boolean isAsync) {
+        collectMetric(plugin, gaugeBuilder(name, help), supplier, isAsync);
+    }
 }
